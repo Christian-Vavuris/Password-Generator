8af072e55371522164b88ee2e2df4d4ae3f54a49
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-06-03 18:56:35 -0700

    created random number generator to assign length

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,20 @@
+// PseudoCode
+// Requested features - 
+//   random length between 8 and 128
+//   choose between lowercase, uppercase, numeric, and special characters
+//   checkbox list of features to include
+//   there can't be no character types selected
+//   password needs to meet the selected criteria
+//   password needs to display as an alert or bee written to the pageXOffset
+
 // Assignment code here
+
+// 
+var randomLength = function (length) {
+  var characters = Math.round(Math.random() * length);
+  return characters;
+}
+
 
 
 // Get references to the #generate element
@@ -14,4 +30,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+
+randomLength(128);
+console.log(randomLength(128))
